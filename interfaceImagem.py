302e980a9db006ff686f0018af3be1a4fbadf32b
Unknown <jaquelinedonin@gmail.com>
2019-05-04 16:26:35 -0300

    Atualização 01
    Add filtro negativo

--- interfaceImagem.py
+++ interfaceImagem.py
@@ -8,9 +8,10 @@ import cv2
 
 import numpy as np
 
+global ImgSave
 
 # Here, we are creating our class, Window, and inheriting from the Frame
-# class. Frame is a class from the tkinter module. (see Lib/tkinter/__init__)
+# class. Frame is a class from the tkinter module. 
 class Window(Frame):
 
     # Define settings upon initialization. Here you can specify
@@ -21,9 +22,10 @@ class Window(Frame):
 
         #reference to the master widget, which is the tk window                 
         self.master = master
-
+        
         #with that, we want to then run init_window, which doesn't yet exist
         self.init_window()
+        #show image in window
         self.showImg("Kanban-menor.png")
 
     #Creation of init_window
@@ -39,180 +41,184 @@ class Window(Frame):
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro Gray"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterGray
-        self.autenticar.pack(side=RIGHT)
+        self.autenticar.pack(side = RIGHT)
         #Button green filter
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro Green"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterGreen
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
+        self.autenticar.pack(side = "left")
+        #Button filter2D
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro 2d"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filter2D
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
+        self.autenticar.pack(side = TOP)
+        #Button  filter Blur
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro Blur"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterBlur
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
+        self.autenticar.pack(side = RIGHT)
+        #Button Gaussian filter
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "FiltroGaussian"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterGaussianBlur
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
-        self.autenticar = Button(self.master)
-        self.autenticar["text"] = "Filtro Menian"
-        self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
-        self.autenticar["command"] = self.filterMedian
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
+        self.autenticar.pack(side = "left")
+        #Button Bilateral filter
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro Bilateral"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterBilateral
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
+        self.autenticar.pack(side ="bottom")
+        #Button Blue filter
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro Blue"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterBlue
-        self.autenticar.pack(side=RIGHT)
-        #Button green filter
+        self.autenticar.pack(side = "bottom")
+        #Button Red filter
         self.autenticar = Button(self.master)
         self.autenticar["text"] = "Filtro Red"
         self.autenticar["font"] = ("Calibri", "8")
-        self.autenticar["width"] = 12
+        self.autenticar["width"] = 8
         self.autenticar["command"] = self.filterRed
-        self.autenticar.pack(side=RIGHT)
+        self.autenticar.pack(side = "bottom")
+        #Button Negative filter
+        self.autenticar = Button(self.master)
+        self.autenticar["text"] = "Filtro Negativo"
+        self.autenticar["font"] = ("Calibri", "8")
+        self.autenticar["width"] = 8
+        self.autenticar["command"] = self.filterNegativo
+        self.autenticar.pack(side = "bottom")
 
+    #Creating the function showImg
     def showImg(self, image):
         load = Image.open(image)
         render = ImageTk.PhotoImage(load)
-        # labels can be text or images
         img = Label(self, image=render)
         img.image = render
-        img.place(x=0, y=0)
-
-
+        img.place(x=50, y=10)
+    
     def filterGray(self):
         img = cv2.imread('Kanban-menor.png')
         #Applying Grayscale filter to image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #Saving filtered image to new file
         cv2.imwrite('graytest.png',gray)
-        #showing the new image in gray scale
+        ImgSave = gray
+        #showing the new image 
         self.showImg("graytest.png")
 
     def filter2D(self):
         image = cv2.imread('Kanban-menor.png')
-        #Applying Grayscale filter to image
+        #Applying kernel filter to image
         kernel = np.ones((6,6),np.float32)/25
         filter2D = cv2.filter2D(image,-1,kernel)
         #Saving filtered image to new file
         cv2.imwrite('filter2D.png',filter2D)
-        #showing the new image in gray scale
+        ImgSave = filter2D
+        #showing the new image 
         self.showImg("filter2D.png")
 
     def filterBlur(self):
         image = cv2.imread('Kanban-menor.png')
-        #Applying Grayscale filter to image
+        #Applying Blur filter to image
         filterBlur = cv2.blur(image,(5,5))
         #Saving filtered image to new file
         cv2.imwrite('filterBlur.png',filterBlur)
-        #showing the new image in gray scale
+        #showing the new image 
         self.showImg("filterBlur.png")
 
     def filterGaussianBlur(self):
         image = cv2.imread('Kanban-menor.png')
-        #Applying Grayscale filter to image
+        #Applying Gaussian Blur filter to image
         gaussianBlur = cv2.GaussianBlur(image,(5,5),0)
         #Saving filtered image to new file
         cv2.imwrite('gaussianBlur.png',gaussianBlur)
-        #showing the new image in gray scale
+        #showing the new image 
         self.showImg("gaussianBlur.png")
-
-    def filterMedian(self):
-        image = cv2.imread('Kanban-menor.png')
-        #Applying Grayscale filter to image
-        median = cv2.medianBlur(image,5)
-        #Saving filtered image to new file
-        cv2.imwrite('median.png',median)
-        #showing the new image in gray scale
-        self.showImg("filterblur.png")
 
     def filterBilateral(self):
         image = cv2.imread('Kanban-menor.png')
-        #Applying Grayscale filter to image
+        #Applying Bilateral filter to image
         bilateralFilter = cv2.bilateralFilter(image,9,75,75)
         #Saving filtered image to new file
         cv2.imwrite('bilateralFilter.png',bilateralFilter)
-        #showing the new image in gray scale
+        #showing the new image 
         self.showImg("bilateralFilter.png")
 
     def filterGreen(self):
         imagem = cv2.imread('Kanban-menor.png')
-        #percorre as linhas
-        for y in range(0, imagem.shape[0], 1):
-        #percorre as colunas 
-         for x in range(0, imagem.shape[1], 1):
-            #Lê cada pixel de imagem e adiciona as cores nas variáveis 
-            (b, g, r) = imagem[y, x]
-            #Adiciona a imagem apenas o pixel de cor verde 
-            imagem[y, x] = (b%1,g%256,r%1)
-        #salva a imagem modificada na pasta 
+        #Reading the lines of image
+        for linha in range(0, imagem.shape[0], 1):
+        #Reading the collumns 
+         for coluna in range(0, imagem.shape[1], 1):
+            #Reading each pixel and add to variables the colors
+            (b, g, r) = imagem[linha,coluna]
+            #Add just the color blue in the picture  
+            imagem[linha,coluna] = (b%1,g%256,r%1)
+        #save the modify picture 
         cv2.imwrite("filterGreen.png", imagem)
-        #showing the new image in gray scale
+        #showing the new image 
         self.showImg("filterGreen.png")
 
     def filterBlue(self):
         imagem = cv2.imread('Kanban-menor.png')
-        #percorre as linhas
-        for y in range(0, imagem.shape[0], 1):
-        #percorre as colunas 
-         for x in range(0, imagem.shape[1], 1):
-            #Lê cada pixel de imagem e adiciona as cores nas variáveis 
-            (b, g, r) = imagem[y, x]
-            #Adiciona a imagem apenas o pixel de cor verde 
-            imagem[y, x] = (b%1,g%256,r%1)
-        #salva a imagem modificada na pasta 
+        #Reading the lines
+        for linha in range(0, imagem.shape[0], 1):
+        #Reading the collumns 
+         for coluna in range(0, imagem.shape[1], 1):
+            #Reading each pixel and add to variables the colors
+            (b, g, r) = imagem[linha,coluna]
+            #Add just the color blue in the picture  
+            imagem[linha,coluna] = (b%256,g%1,r%1)
+        #save the modify picture
         cv2.imwrite("filterBlue.png", imagem)
-        #showing the new image in gray scale
+        #showing the new image 
         self.showImg("filterBlue.png")
 
     def filterRed(self):
         imagem = cv2.imread('Kanban-menor.png')
-        #percorre as linhas
-        for y in range(0, imagem.shape[0], 1):
-        #percorre as colunas 
-         for x in range(0, imagem.shape[1], 1):
-            #Lê cada pixel de imagem e adiciona as cores nas variáveis 
-            (b, g, r) = imagem[y, x]
-            #Adiciona a imagem apenas o pixel de cor verde 
-            imagem[y, x] = (b%1,g%256,r%1)
-        #salva a imagem modificada na pasta 
+        #Reading the lines
+        for linha in range(0, imagem.shape[0], 1):
+        #Reading the collumns 
+         for coluna in range(0, imagem.shape[1], 1):
+            #Reading each pixel and add to variables the colors
+            (b, g, r) = imagem[linha,coluna]
+            #Add just the color blue in the picture  
+            imagem[linha,coluna] = (b%1,g%1,r%256)
+        #save the modify picture
         cv2.imwrite("filterRed.png", imagem)
-        #showing the new image in gray scale
+        #showing the new image 
+        self.showImg("filterRed.png")
+
+    def filterNegative(self):
+        imagem = cv2.imread('Kanban-menor.png')
+             
+        #Reading the lines
+        for linha in range(0, imagem.shape[0], 1):
+        #Reading the collumns 
+         for coluna in range(0, imagem.shape[1], 1):
+            #Reading each pixel and add to variables the colors
+            (b, g, r) = imagem[linha,coluna]
+            #Add just the color blue in the picture  
+            imagem[linha,coluna] = (255 - b,255 - g,255 - r)
+        #save the modify picture
+        cv2.imwrite("filterRed.png", imagem)
+        #showing the new image 
         self.showImg("filterRed.png")
         
-
-    def client_exit(self):
-        exit()
-
-
+    
 # root window created. Here, that would be the only window, but
 # you can later have windows within windows.
 root = Tk()
@@ -221,7 +227,6 @@ root.geometry("500x500")
 
 #creation of an instance
 app = Window(root)
-
 
 #mainloop 
 root.mainloop()
